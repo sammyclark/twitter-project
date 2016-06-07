03c2226f073e9978035d8b8a45ba24b4a552b9cf
sammyclark <iamsam10@hotmail.co.uk>
2016-06-07 20:40:08 +0100

    Started building
    while loop so that you can type stuff

---
+++ tc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String stuff;
+            Console.WriteLine("Type shit");
+            stuff = Console.ReadLine();
+          
+
+            while (stuff != "exit")
+            {
+                Console.WriteLine(stuff);
+               stuff = Console.ReadLine();
+            }               
+        }
+    }
+}
